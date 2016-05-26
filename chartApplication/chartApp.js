b839e46f14662710c8de0cd118852a621e142f3d
mic <mic.excel@gmail.com>
2016-05-26 17:05:30 +0100

    Update chartApp.js

--- chartApplication/chartApp.js
+++ chartApplication/chartApp.js
@@ -5,6 +5,7 @@ let appCtx = 0;
 let chartDiagram = '';
 let chartLabels = [];
 let chartValues = [];
+let valueTotal =0;
 
 // Check that the browser is compatible with Html5 Canvas
 
@@ -26,7 +27,8 @@ function lastData() {
     let lastEntered= 'Last data entered: ' +  'Label: ' + lastLabel + ', ' + 'Value: ' + lastValues;    
     document.getElementById('feedback').innerHTML = lastEntered;   
     chartLabels.push(lastLabel);
-    chartValues.push(lastValues);
+    chartValues.push(parseInt(lastValues));
+    valueTotal += parseInt(lastValues)
 }
 
 // Accept user command to draw
@@ -47,28 +49,28 @@ function drawSelect(){
     chartValues = [];
 }
 
-
-
-
-
 //Draw cases
 
 function draw(chartType){
     switch(chartType){
-        case 'Pie Chart':            
-            pieChart(chartLabels,[ 0.41887902047863906, 0.8377580409572781, 1.2566370614359172, 1.6755160819145563, 2.0943951023931953 ]);
+        case 'Pie Chart': 
+            function valueAngle(num){
+                return 2*Math.PI*num/valueTotal;
+            }
+          
+            pieChart(chartLabels, chartValues.map(valueAngle));
             break;
 
-        case 'Histogram':            
-            histogram(chartLabels,[120,40,60,80,100,50]);
+        case 'Histogram':     
+            histogram(chartLabels,chartValues);
             break;
 
         case 'Bar Chart':            
-            barChart(chartLabels,[110,40,60,80,100,50]);
+            barChart(chartLabels,chartValues);
             break;
 
         case 'Line Chart':            
-            lineChart(chartLabels,[110,40,60,80,100,50]);
+            lineChart(chartLabels,chartValues);
             break;
 
         //case 'expiechart':
@@ -101,20 +103,41 @@ function pieChart(arr1,arr2){
         appCtx.arc(150, 75, 60, pieStart, pieEnd);
         appCtx.lineTo(150,75);
         appCtx.fill();
+
+        //appCtx.font = '7px arial';
+        //appCtx.fillText(arr1[i], 45+40*i,130);    
     };
+}
+
+// Normalize function ensures that charts stay within the visible Y-axis
+
+function normalize(num,arr2) {
+    return 120*arr2[i]/Math.max.apply(Math,chartValues);
+}
+
+//This function draws the X and Y axes on the canvas
+
+function xYAxis() {
+    appCtx.beginPath();
+    appCtx.moveTo(45,0);
+    appCtx.lineTo(45,120);
+    appCtx.lineTo(300,120);
+    appCtx.strokeStyle = 'black';
+    appCtx.stroke();
+    appCtx.moveTo(45,0);
 }
 
 //Histogram
 
-function histogram(arr1, arr2){
+function histogram(arr1, arr2){   
+    xYAxis();
     for(i=0; i<arr2.length; i++){
-        appCtx.beginPath();
+        
+        //appCtx.strokeStyle = 'rgba(' + Math.floor(255-128*i/arr2.length) +',' + Math.floor(128*i/arr2.length) + ',' + Math.floor(255*i/arr2.length) + ',1)';
 
-        //appCtx.strokeStyle = 'rgba(' + Math.floor(255-128*i/arr.length) +',' + Math.floor(128*i/arr.length) + ',' + Math.floor(255*i/arr.length) + ',1)';
+        appCtx.fillStyle = 'rgba(' + Math.floor(255-128*i/arr2.length) +',' + Math.floor(128*i/arr2.length) + ',' + Math.floor(255*i/arr2.length) + ',1)';        
 
-        //appCtx.fillStyle = 'rgba(' + Math.floor(255-128*i/arr.length) +',' + Math.floor(128*i/arr.length) + ',' + Math.floor(255*i/arr.length) + ',1)';
-
-        appCtx.strokeRect(45+40*i,120-arr2[i],40,arr2[i]);
+        appCtx.fillRect(45+40*i,120-normalize(i, arr2),40,normalize(i, arr2));        
         appCtx.font = '7px arial';
         appCtx.fillText(arr1[i], 45+40*i,130);
     };
@@ -123,12 +146,10 @@ function histogram(arr1, arr2){
 //Bar Chart
 
 function barChart(arr1, arr2){
+    xYAxis();
     for(i=0; i<arr2.length; i++){
-        appCtx.beginPath();
-
-        //appCtx.fillStyle ='cyan';
-
-        appCtx.fillRect(45+40*i,120-arr2[i],30,arr2[i]);
+        appCtx.fillStyle = 'rgba(' + Math.floor(255-128*i/arr2.length) +',' + Math.floor(128*i/arr2.length) + ',' + Math.floor(255*i/arr2.length) + ',1)';   
+        appCtx.fillRect(45+40*i,120-normalize(i, arr2),30,normalize(i, arr2));
         appCtx.font = '7px arial';
         appCtx.fillText(arr1[i], 45+40*i,130);
     };
@@ -137,62 +158,14 @@ function barChart(arr1, arr2){
 //Line Chart
 
 function lineChart(arr1, arr2){
-    appCtx.beginPath();
-    appCtx.moveTo(45,120-arr2[0]);
+    xYAxis();    
+    appCtx.moveTo(45,120-normalize(0, arr2));
     for(i=0; i<arr2.length; i++){        
-        appCtx.lineTo(45+40*i,120-arr2[i]);
+        appCtx.lineTo(45+40*i,120-normalize(i, arr2));
         appCtx.stroke();
-
-        //appCtx.fillStyle ='cyan';        
-
+        appCtx.strokeStyle= 'blue';
         appCtx.font = '7px arial';
         appCtx.fillText(arr1[i], 45+40*i,130);
-        appCtx.fillText(arr1[i], 45+40*i,120-arr2[i]);
+        appCtx.fillText(arr1[i], 45+40*i,120-normalize(i, arr2));
     };
 }
-
-//Exploded Pie Chart
-
-function exPieChart(arr){
-    let pieStart = 0;
-    let pieEnd = 0;
-    for(i=0; i<arr.length; i++){
-        pieStart = pieEnd;
-        pieEnd += arr[i];
-
-       // appCtx.strokeStyle = rgba(10+40*i,255-40*i,0,1);
-
-        appCtx.beginPath();
-        appCtx.lineTo(150,75);
-        appCtx.arc(150, 75, 60, pieStart, pieEnd);
-        appCtx.lineTo(150,75);
-        appCtx.stroke();
-    };
-    appCtx.beginPath();
-    appCtx.lineTo(180,75);
-    appCtx.arc(180, 75, 60, 0, 2*Math.PI/15);
-    appCtx.lineTo(180,75);
-    appCtx.stroke();
-}
-
-
-
-// Input
-
-
-var a = [1,2,3,4,5];
-
-
-function addValues(arr1,arr2){
-    return arr1+arr2;
-} 
-
-var valueTotal= a.reduce(addValues)
-
-function valueAngle(arr){
-    return 2*Math.PI*arr/valueTotal;
-}
-
-
-var b= a.map(valueAngle);
-
